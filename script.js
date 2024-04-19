ad6292fb553b3ba7ac632bfc3a5e6da7b9540c36
cdanderson76 <138369806+cdanderson76@users.noreply.github.com>
2024-04-18 23:05:03 -0500

    Added Click Event
    Just finished adding my click event to the mobile view of the application.  This is a very rough draft that I will revisit, but for now, I'm going to start working on the responsiveness and jump to the desktop version of this project

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+let display = true;
+let cont = true
 
-const shareBtn = document.getElementById('share-btn');
+const lightShareBtn = document.getElementById('share-btn');
+const darkShareBtn = document.getElementById('dark-share');
 
 const nameDate = document.getElementById('name-date-container');
 const profilePic = document.querySelector('.michelle');
-const sigSect = document.getElementById('sig-section');
+const sigSect = document.getElementById('sig-section-event');
 
-shareBtn.addEventListener('click', function() {
+lightShareBtn.addEventListener('click', function() {
   nameDate.style.visibility = 'hidden';
   profilePic.style.visibility = 'hidden';
-  sigSect.style.backgroundColor = '#48556A'
+  sigSect.style.visibility = 'visible';
+})      
+ 
+darkShareBtn.addEventListener('click', function() {
+    nameDate.style.visibility = 'visible';
+    profilePic.style.visibility = 'visible';
+    sigSect.style.visibility = 'hidden';
 })
+
